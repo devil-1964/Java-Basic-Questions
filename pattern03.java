0c521d326d5eeec1051dc802195227b58e88b981
Devansh <98086933+devil-1964@users.noreply.github.com>
2022-08-21 14:58:25 +0530

    Add files via upload

---
+++ pattern03.java
@@ -0,0 +1,34 @@
+import java.util.*;
+
+class pattern03 {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int nr = sc.nextInt();
+        int row, nst, cst, nsp, csp;
+        row = 1;
+        nst = 1;
+        nsp = nr/2;
+        while (row <= nr) {
+            csp = 1;
+            while (csp <= nsp ) {
+                System.out.print("  ");
+                csp++;
+            }
+            cst = 1;
+            while (cst <= nst) {
+                System.out.print("* ");
+                cst++;
+            }
+            if (row <= nr / 2) {
+                nst+=2;
+                nsp--;
+            } else {
+                nst-=2;
+                nsp++;
+            }
+            System.out.println("");
+            row++;
+        }
+
+    }
+}
