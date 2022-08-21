fcec6b676cd591b1c01313fa75175839cc4cd942
Devansh <98086933+devil-1964@users.noreply.github.com>
2022-08-21 15:08:28 +0530

    Add files via upload

---
+++ pattern05.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+class pattern05 {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int row, nst, cst;
+        row = 1;
+        nst = n;
+        while (row <= n) {
+            cst = 1;
+            while (cst <= nst) {
+                if (cst == 1 || cst == n || row == 1 || row == n) {
+                    System.out.print("* ");
+                } else {
+                    System.out.print("  ");
+                }
+                cst++;
+            }
+            System.out.println("");
+            row++;
+        }
+
+    }
+}
