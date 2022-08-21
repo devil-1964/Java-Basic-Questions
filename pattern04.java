9d487cd13a842965775ae90b0ce639acb050d724
Devansh <98086933+devil-1964@users.noreply.github.com>
2022-08-21 15:02:42 +0530

    Add files via upload

---
+++ pattern04.java
@@ -0,0 +1,23 @@
+import java.util.*;
+class pattern04
+{
+    public static void main(String args[]) {
+        Scanner sc= new Scanner(System.in);
+        int n = sc.nextInt();
+        int row,nst,cst;
+        row=1;
+        nst=n;
+        while(row<=n)
+        {
+            cst=1;
+            while(cst<=nst)
+            {
+                System.out.print("* ");
+                cst++;
+            }
+            System.out.println("");
+            row++;
+        }
+        
+    }
+}
